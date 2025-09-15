284137be00868a31a2ca234592a525351d2a87c5
678-bhumika <bhumika852004@gmail.com>
2025-09-15 14:37:02 +0530

    updated CoursesSection

--- client/src/components/learner/CoursesSection.jsx
+++ client/src/components/learner/CoursesSection.jsx
@@ -14,11 +14,11 @@ const CoursesSection = () => {
       <p className="text-sm md:text-base text-gray-500 mt-3">Discover top-rated courses across various categories. From coding and design to <br/>business and wellness, our courses are crafted to deliver results.</p>
  
         {/*displays only 4 courses on the homepage*/}
-      <div className="grid grid-cols-auto px-4 md:px-0 md:my-16 my-10 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 px-4 md:grid-cols-4  my-10 gap-4">
         {allCourses.slice(0,4).map((course, index)=> <CourseCard key={index} course={course} />)}
       </div>
 
-      <Link to={'/course-list'} onClick={()=> scrollTo(0,0)}
+      <Link to={'/course-list'} onClick={()=> window.scrollTo(0,0)}
       className="text-gray-500 border border-gray-500/30 px-10 py-3 rounded "
       >
       Show all courses</Link>
