27e82dd9cbd2708260c3f1c45d3308433c408fd6
678-bhumika <bhumika852004@gmail.com>
2025-09-25 13:20:38 +0530

    test page

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+// In app/test/page.tsx
+export default function TestPage() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center", fontSize: "24px" }}>
+      <h1>Test Page Works!</h1>
+      <p>If you can see this, basic routing is configured correctly.</p>
+    </div>
+  );
+}
